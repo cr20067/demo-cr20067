01c485837c2cf7dc6c9c6336cd6244dde8e6e8f1
cr20067 <cr20067@ues.edu.sv>
2024-05-05 03:44:36 -0600

    Mi primer comentario

---
+++ src/main/java/com/iis/cr20067/App.java
@@ -0,0 +1,17 @@
+package com.iis.cr20067;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
